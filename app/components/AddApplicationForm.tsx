3bcc9dc7b4bfbc80c3c9b739c25a6ad1cad65c3c
Elliot <unforriven@gmail.com>
2025-11-25 18:49:42 -0300

    Add: basic form to create applications

---
+++ app/components/AddApplicationForm.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { useState } from "react"
+import { supabase } from "@/lib/supabase"
+
+export default function AddApplicationForm() {
+    const [company, setCompany] = useState('')
+    const [position, setPosition] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+            const { error } = await supabase
+                .from('applications')
+                .insert([{ company, position }])
+
+            if (error) throw error
+
+            //Reset Form
+
+            setCompany('')
+            setPosition('')
+            alert('Application added!')
+        } catch (error) { 
+            console.error('Error: ', error)
+            alert('Error adding appliaction')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="max-w-md space-y-4">
+            <div>
+                <label className="block text-sm font-medium mb-1 text-zinc-300 ">
+                    Company
+                </label>
+                <input
+                    type="text"
+                    value={company}
+                    onChange={(e) => setCompany(e.target.value)}
+                    required
+                    className="w-full px-3 py-2 border rounded-lg outline-none text-zinc-500"
+                />
+            </div>
+
+            <div>
+                <label className="block text-sm font-medium mb-1 text-zinc-300">
+                    Position
+                </label>
+                <input
+                    type="text"
+                    value={position}
+                    onChange={(e) => setPosition(e.target.value)}
+                    required
+                    className="w-full px-3 py-2 border rounded-lg outline-none text-zinc-500"
+                />
+            </div>
+
+            <button
+                type="submit"
+                disabled={loading}
+                className="w-full cursor-pinter bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 disabled:opacity-50"
+            >
+
+                {loading ? 'Adding...' : 'Add Application'}
+            </button>
+        </form>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import Image from "next/image";
+import AddApplicationForm from "./components/AddApplicationForm";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-zinc-50 font-sans dark:bg-[#111]">
       <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
         Welcome to Job Tracker
       </h1>
-      <p className="text-lg text-zinc-600 dark:text-zinc-400">Track your job applications efficiently</p>
+      <p className="text-lg text-dark dark:text-zinc-400">Track your job applications efficiently</p>
+      <AddApplicationForm />
     </div>
   );
 }
